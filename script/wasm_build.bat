acc179a52820b4b8ea89a2cad8a4bbdd5c3acc8b
Mizar <mizar.jp@gmail.com>
2022-03-05 11:01:42 +0900

    - lock emsdk:3.1.3

--- script/wasm_build.bat
+++ script/wasm_build.bat
@@ -1,5 +1,5 @@
 @echo off
 cd %~dp0
 cd ..
-docker pull emscripten/emsdk
-docker run --rm -v %CD%:/src emscripten/emsdk npm run-script build
+rem docker pull emscripten/emsdk
+docker run --rm -v %CD%:/src emscripten/emsdk:3.1.3 npm run-script build

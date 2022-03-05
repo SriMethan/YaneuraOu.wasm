91262cfaad52c76f7557886e4b6ff3cd65105ee9
Mizar <mizar.jp@gmail.com>
2022-03-05 20:26:23 +0900

    - emscripten/emsdk:latest

--- script/wasm_build.bat
+++ script/wasm_build.bat
@@ -1,5 +1,5 @@
 @echo off
 cd %~dp0
 cd ..
-rem docker pull emscripten/emsdk
-docker run --rm -v %CD%:/src emscripten/emsdk:3.1.2 npm run-script build
+docker pull emscripten/emsdk:latest
+docker run --rm -v %CD%:/src emscripten/emsdk:latest npm run-script build

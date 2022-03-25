360cd3f4e869d0942e3664be453d759fb9857824
Mizar <mizar.jp@gmail.com>
2022-03-26 08:49:15 +0900

    - npmpackages bin/lib

---
+++ npmpackages/yaneuraou.halfkp.noeval/lib/yaneuraou.halfkp.noeval.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="emscripten" />
+import { YaneuraOuModule } from "./yaneuraou.module";
+
+declare const YaneuraOu_HalfKP_noeval: EmscriptenModuleFactory<YaneuraOuModule>;
+export = YaneuraOu_HalfKP_noeval;
---
+++ npmpackages/yaneuraou.material9/lib/yaneuraou.material9.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="emscripten" />
+import { YaneuraOuModule } from "./yaneuraou.module";
+
+declare const YaneuraOu_Material9: EmscriptenModuleFactory<YaneuraOuModule>;
+export = YaneuraOu_Material9;

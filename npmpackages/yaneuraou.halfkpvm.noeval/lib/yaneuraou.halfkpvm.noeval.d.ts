e107aabaaf6209a85f5d8e08ac9024e8226eaeab
Mizar <mizar.jp@gmail.com>
2022-03-26 08:49:15 +0900

    - npmpackages bin/lib

---
+++ npmpackages/yaneuraou.halfkpvm.noeval/lib/yaneuraou.halfkpvm.noeval.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="emscripten" />
+import { YaneuraOuModule } from "./yaneuraou.module";
+
+declare const YaneuraOu_HalfKPvm_noeval: EmscriptenModuleFactory<YaneuraOuModule>;
+export = YaneuraOu_HalfKPvm_noeval;

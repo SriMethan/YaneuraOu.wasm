b8a1d133665fb8a7295cf13fff875b05a94c7870
Mizar <mizar.jp@gmail.com>
2022-04-01 00:16:53 +0900

    - npmpackages bin/lib

---
+++ npmpackages/yaneuraou.komoringheights-mate/lib/yaneuraou.komoringheights-mate.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="emscripten" />
+import { YaneuraOuModule } from "./yaneuraou.module";
+
+declare const KomoringHeights_MATE: EmscriptenModuleFactory<YaneuraOuModule>;
+export = KomoringHeights_MATE;
